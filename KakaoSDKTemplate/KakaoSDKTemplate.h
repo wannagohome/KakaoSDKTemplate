3f1d039c5cf82517439cb2e016b5218af86fa62b
wannagohome <iwwuf7@gmail.com>
2020-09-24 15:49:37 +0900

    Initial Commit

---
+++ KakaoSDKTemplate/KakaoSDKTemplate.h
@@ -0,0 +1,19 @@
+//
+//  KakaoSDKTemplate.h
+//  KakaoSDKTemplate
+//
+//  Created by jinho jang on 2020/09/24.
+//  Copyright © 2020 People. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KakaoSDKTemplate.
+FOUNDATION_EXPORT double KakaoSDKTemplateVersionNumber;
+
+//! Project version string for KakaoSDKTemplate.
+FOUNDATION_EXPORT const unsigned char KakaoSDKTemplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KakaoSDKTemplate/PublicHeader.h>
+
+
